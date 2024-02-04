77b29859e1ace9b28656ecf1ea9254253ceffd38
CoCo9122 <banaji.yu0517@outlook.jp>
2024-02-04 16:16:30 +0900

    IoUの修正

--- train/metrics/metrics.py
+++ train/metrics/metrics.py
@@ -8,6 +8,13 @@ def dice_coeff(y_true, y_pred, smooth = 1):
     return score
 
 def iou(y_true, y_pred):
-    i = tf.reduce_sum(y_pred*y_true, axis=(1, 2))
-    u = tf.reduce_sum(y_pred + y_true, axis=(1, 2)) - i
-    return tf.reduce_mean(i / u)
+    # しきい値を適用してバイナリマスクを得るか、既にバイナリマスクであることを確認する
+    y_pred_bin = tf.cast(y_pred > 0.5, tf.float32)
+    y_true_bin = tf.cast(y_true > 0.5, tf.float32)
+
+    # インターセクションとユニオンの計算
+    i = tf.reduce_sum(y_pred_bin * y_true_bin, axis=(1, 2))
+    u = tf.reduce_sum(tf.cast(y_pred_bin + y_true_bin > 0, tf.float32), axis=(1, 2))
+
+    # 0で割ることを避けるために安全な割り算を使用
+    return tf.reduce_mean(i / (u + tf.keras.backend.epsilon()))
