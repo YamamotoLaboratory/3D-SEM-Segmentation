0c9f247458f4d19074fea5941fade221d4897e35
CoCo9122 <banaji.yu0517@outlook.jp>
2024-03-05 17:55:30 +0900

    last fix

--- log/logging.py
+++ log/logging.py
@@ -7,6 +7,6 @@ import yaml
 def load_logging_config(name):
     config.dictConfig(yaml.load(open('{}/log/logging_setting.yaml'.format(os.getcwd())).read(), Loader=yaml.SafeLoader))
     logger = getLogger(name)
-    logger.debug('デバッグモードでプログラムが実行されました。')
+    logger.debug('デバッグモードにてプログラムが実行されました。')
     logger.info('ログ設定の読み込みが完了しました。')
     return logger
